8f2b180a49b46a72e2ed75efe620bd65b0cad192
perera2001 <nandunperera2001@gmail.com>
2025-09-14 13:15:03 +0530

    Backend controller created

--- weatherapp/src/main/java/com/example/weatherapp/controller/WeatherController.java
+++ weatherapp/src/main/java/com/example/weatherapp/controller/WeatherController.java
@@ -1,4 +1,20 @@
 package com.example.weatherapp.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class WeatherController {
+    @GetMapping("api/weather")
+    public List<Map<String,Object>> getWeather() {
+          return List.of(
+                  Map.of("name","Colombo","weather",List.of(Map.of("description","Sunny")),"main",Map.of("temp","30")),
+                  Map.of("name","Tokyo","weather",List.of(Map.of("description","Cloudy")),"main",Map.of("temp",22))
+
+          );
+    }
 }
